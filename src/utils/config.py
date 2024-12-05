0767a9ad89032072a1eacf939161d48972c7cc24
Nfx1z <mahadikanafiz86@gmail.com>
2024-12-05 17:54:40 +0700

    Delete: var EXPIRE

--- src/utils/config.py
+++ src/utils/config.py
@@ -10,6 +10,3 @@ DB_IP=os.getenv('DB_IP')
 
 # JWT authentication
 JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY')
-
-# Expired token after 15 minutes
-EXPIRES = datetime.datetime.utcnow() + datetime.timedelta(seconds=30)
